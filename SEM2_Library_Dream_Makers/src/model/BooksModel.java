fc36b67189ad3b4d5ba27e5089c23b41e70f30f7
APTECH <APTECH@192.168.2.154>
2020-10-09 09:56:06 +0700

    update booksmodel

--- SEM2_Library_Dream_Makers/src/model/BooksModel.java
+++ SEM2_Library_Dream_Makers/src/model/BooksModel.java
@@ -18,27 +18,84 @@ public class BooksModel {
 		try {
 			PreparedStatement preparedStatement = new ConnectDB().getConnection()
 					.prepareStatement("SELECT * FROM books ");
-			
+
 			ResultSet resultSet = preparedStatement.executeQuery();
-			
+
 			while (resultSet.next()) {
 				Books book = new Books();
 				book.setBook_ID(resultSet.getString("book_ID"));
 				book.setCall_number(resultSet.getString("call_number"));
-				book.setIsbn(resultSet.getString("isbn")); 
+				book.setIsbn(resultSet.getString("isbn"));
 				book.setTitle(resultSet.getString("title"));
 				book.setPublish_ID(resultSet.getString("publish_ID"));
 				book.setCategory_ID(resultSet.getInt("category_ID"));
-				book.setQuantity(resultSet.getInt("quantity"));				
+				book.setQuantity(resultSet.getInt("quantity"));
 				books.add(book);
+
 			}
-			
 		} catch (Exception e) {
+			// TODO: handle exception
 			System.err.println(e.getMessage());
 			books = null;
 		}
 
 		return books;
+	}
+	
+	public static boolean create(Books book ) {
+
+		try {
+			PreparedStatement preparedStatement = new ConnectDB().getConnection().prepareStatement(
+					" insert into books(book_ID,call_number,isbn,title,category_ID,publish_ID,quantity) values(?,?,?,?,?,?,?) ");
+			
+			preparedStatement.setString(1, book.getBook_ID());
+			preparedStatement.setString(2, book.getCall_number());
+			preparedStatement.setString(3, book.getIsbn());
+			preparedStatement.setString(4, book.getTitle());
+			preparedStatement.setInt(5, book.getCategory_ID());
+			preparedStatement.setString(6, book.getPublish_ID());
+			preparedStatement.setInt(7, book.getQuantity());
+			return preparedStatement.executeUpdate() > 0;
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.err.println(e.getMessage());
+			return false;
+		}
+	}
+	
+	public static boolean update(Books book, String book_ID) {
+
+		try {
+			PreparedStatement preparedStatement = new ConnectDB().getConnection().prepareStatement(
+					" update books set call_number = ? , isbn = ? , title = ? , category_ID = ? , publish_ID = ? , quantity = ? where book_ID = ?  ");
+			preparedStatement.setString(1, book.getCall_number());
+			preparedStatement.setString(2, book.getIsbn());
+			preparedStatement.setString(3, book.getTitle());
+			preparedStatement.setInt(4, book.getCategory_ID());
+			preparedStatement.setString(5, book.getPublish_ID());
+			preparedStatement.setInt(6, book.getQuantity());
+			preparedStatement.setString(7, book_ID );
+			return preparedStatement.executeUpdate() > 0;
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.err.println(e.getMessage());
+			return false;
+		}
+	}
+	
+	public static boolean delete(String book_ID) {
+		try {
+			PreparedStatement preparedStatement = new ConnectDB().getConnection()
+					.prepareStatement("delete from books where book_ID = ? ") ;
+			preparedStatement.setString(1, book_ID );
+			return preparedStatement.executeUpdate() > 0 ; 
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.err.println(e.getMessage());
+			return false;
+		}
+		 	
 	}
 
 	public Books find(String id) {
@@ -59,11 +116,52 @@ public class BooksModel {
 				book.setQuantity(resultSet.getInt("quantity"));
 			}			
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			System.err.println(e.getMessage());
-			book = null ; 
+			book = null ;  
 		} 		
 		
 		return book;
+		
+	}
+	public String findPublish(String publish_ID) {
+		String name = null ; 
+		try {
+			PreparedStatement preparedStatement = new ConnectDB().getConnection().prepareStatement(
+					" select * from publish_house where publish_ID = ?  ");	
+			preparedStatement.setString(1, publish_ID );
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			if (resultSet.next()) {
+				name = resultSet.getString("name") ;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.err.println(e.getMessage() + " 456");
+			name = null ;
+		}
+		return name;
+		
+		
+	}
+	public String findCategory(int category_ID) {
+		String name = null ; 
+		try {
+			PreparedStatement preparedStatement = new ConnectDB().getConnection().prepareStatement(
+					" select * from category where category_ID = ?  ");	
+			preparedStatement.setInt(1, category_ID );
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			if (resultSet.next()) {
+				name = resultSet.getString("name") ;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.err.println(e.getMessage() + " 123");
+			name = null ;
+		}
+		return name;
+		
 		
 	}
 }
